0740dc4ac1794532040c446897d0ca7d5ab0d84f
Milica Jaksic <milica.jaksic14@gmail.com>
2024-03-26 15:48:14 +0100

    Napravljena Home stranica

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <p>Welcome to the Document Management System of XYZ! To use this system, make sure to
+                first make an account, then log in.
+            </p>    
+        </div>
+    )
+}
